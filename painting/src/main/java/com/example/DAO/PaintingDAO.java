f16f290bfa2a4dc1e512b9b97fd6ac31ebba7a96
mikiemapo <findmikemm@gmail.com>
2023-09-02 02:18:42 +0000

    Removed nested repository

--- painting/src/main/java/com/example/DAO/PaintingDAO.java
+++ painting/src/main/java/com/example/DAO/PaintingDAO.java
@@ -70,7 +70,7 @@ public class PaintingDAO {
             PreparedStatement ps = conn.prepareStatement("select * from painting");
             ResultSet rs = ps.executeQuery();
             /**
-             * if we dont have a value in the rs, return null
+             * if we dont have a value in the rs, return nul
              */
             while(rs.next()){
                 paintings.add(new Painting(rs.getInt("id"), rs.getString("title"), rs.getString("author"), rs.getInt("year_made")));
